24ca8f6ba6010640dc3af7bda287a90994588d42
Daria Czupreta <daria.czupreta@gmail.com>
2024-06-21 17:23:35 +0200

    ring component prototype

--- app/screens/Home.tsx
+++ app/screens/Home.tsx
@@ -6,10 +6,18 @@ import H4Text from "../components/H4Text";
 import Avatar from "../components/Avatar";
 import { useNavigation } from "@react-navigation/native";
 import { MaterialIcons } from "@expo/vector-icons";
+import { Rings } from "../components/SkiaProgressComponents/Rings";
 
 const Home = () => {
   const navigation = useNavigation<any>();
   const [notifications, setNotifications] = useState<boolean>(false);
+
+  return (
+    <View className="h-full">
+      <Rings />
+    </View>
+  );
+
   return (
     <SafeAreaView className="flex-1  bg-primary">
       <View className="flex-row my-5 mx-10">
@@ -41,7 +49,9 @@ const Home = () => {
             </View>
           </TouchableOpacity>
         </View>
-        <View className="flex-row justify-center mt-10"></View>
+        <View className="flex-row justify-center mt-10 bg-white h-64 w-64">
+          <Rings />
+        </View>
       </View>
     </SafeAreaView>
   );
